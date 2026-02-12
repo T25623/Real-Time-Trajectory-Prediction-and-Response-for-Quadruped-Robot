fcfe255010736096144b14c78c3424e36848ed72
20102236 <20102236@mail.wit.ie>
2026-02-12 20:41:42 +0000

    updated x movement logic

--- testing/test.py
+++ testing/test.py
@@ -9,9 +9,13 @@ import threading
 import json
 import time
 import logging
-
+import numpy as np
+from queue import Queue
 
 logging.basicConfig(level=logging.FATAL)
+logging.getLogger("go2_webrtc_driver").setLevel(logging.ERROR)
+logging.getLogger("aiortc").setLevel(logging.ERROR)
+logging.getLogger("aioice").setLevel(logging.ERROR)
 
 
 from hailo_apps.python.pipeline_apps.detection.detection_pipeline import (
@@ -36,6 +40,8 @@ latest_points = None
 mask_state = 0
 latest_center = None
 min_distance = 1000
+lidar_queue = Queue(maxsize=5)
+
 
 BASE = Path(
     "/home/go2/FYP/Real-Time-Trajectory-Prediction-and-Response-for-Quadruped-Robot/testing"
@@ -54,97 +60,65 @@ sys.argv = [
 class AppState(app_callback_class):
     pass
 
-
 def lidar_callback(message):
-    global latest_points, mask_state, latest_center, min_distance
-    #print(message)
+    if not lidar_queue.full():
+        lidar_queue.put(message)
 
-    try:
-        points = np.array(message["data"]["data"]["points"])
-        origin = np.array(message["data"]["origin"])
-        width = np.array(message["data"]["width"])
-        resolution = np.array(message["data"]["resolution"])
+def lidar_distance():
+    global lidar_queue, min_distance
+    
+    while True:
+        message = lidar_queue.get()
 
-        center_x = origin[0] + width[0]/2 * resolution[0]
-        center_y = origin[1] + width[1]/2 * resolution[1]
-        center_z = origin[2] + width[2]/2 * resolution[2]
+        try:
+            data = message["data"]
 
-        x = points[:, 0]
-        y = points[:, 1]
-        z = points[:, 2]
-        
-        mask = 0
-        mask_state = 6
-        #center = points.mean(axis=0)
-        origin_x = origin[0] 
-        origin_y = origin[1]
-        origin_z = origin[2]
-        
-        max_x = np.max(x)
-        max_y = np.max(y)
-        max_z = np.max(z)
 
-        # print(f"max x: {max_x}")
-        # print(f"max y: {max_y}")
-        # print(f"max z: {max_z}")
+            points = np.array(data["data"]["points"], dtype=float)
+            origin = np.array(data["origin"], dtype=float)
+            width = np.array(data["width"], dtype=float)
+            resolution = np.array(data["resolution"], dtype=float)
 
-        min_x = np.min(x)
-        min_y = np.min(y)
-        min_z = np.min(z)
+            min_distance = 1000
 
-        # print(f"min x: {min_x}")
-        # print(f"min y: {min_y}")
-        # print(f"min z: {min_z}")
+            center = origin + (width / 2.0) * resolution
+            center_x, center_y, center_z = center
 
-        # center_x = (max_x - min_x) / 2
-        # center_y = (max_y - min_y) / 2
-        # center_z = (max_z - min_z) / 2
+            x = points[:, 0]
+            y = points[:, 1]
+            z = points[:, 2]
+            
+            mask = 0
+            mask_state = 6
 
-        # for point in points:
-        #     i = (point[0] - origin_x) / 0.05
-        #     j = (point[1] - origin_y) / 0.05
-        #     k = (point[2] - origin_z) / 0.05
+            if mask_state == 0:
+                mask = (np.abs(x+center_x * 0.81) + y+center_y) <= 0
+            elif mask_state == 1:
+                mask = (np.abs(x+center_x * 0.81) - y+center_y) <= 0
+            elif mask_state == 2:
+                mask = (np.abs(y+center_y * 0.81) + x+center_x) <= 0
+            elif mask_state == 3:
+                mask = (np.abs(y+center_y * 0.81) - x+center_x) <= 0
+            elif mask_state == 4:
+                mask = (x+center_x)**2 + (y+center_y)**2 <= 10
+            elif mask_state == 5:
+                mask = (x+center_x)**2 + (y+center_y)**2 >= 2
+            elif mask_state == 6:
+                mask = np.abs(z-1)<=1
+            else:
+                mask = np.ones(len(points), dtype=bool)
 
-        #     if i == 64 and j == 64:
-        #         center_x = point[0]
-        #         center_y = point[1]
-        #         center_z = 0.2
+            filtered_points = points[mask]
+            
+            if len(filtered_points) > 0:
+                distances = np.linalg.norm(filtered_points - np.array([center_x, center_y, center_z]), axis=1)
+                min_distance = float(np.min(distances))
+            
+            #print(f"min distance: {min_distance}")
 
-        print(f"center x: {center_x}")
-        print(f"center y: {center_y}")
-        print(f"center z: {center_z}")
-
-        if mask_state == 0:
-            mask = (np.abs(x+center_x * 0.81) + y+center_y) <= 0
-        elif mask_state == 1:
-            mask = (np.abs(x+center_x * 0.81) - y+center_y) <= 0
-        elif mask_state == 2:
-            mask = (np.abs(y+center_y * 0.81) + x+center_x) <= 0
-        elif mask_state == 3:
-            mask = (np.abs(y+center_y * 0.81) - x+center_x) <= 0
-        elif mask_state == 4:
-            mask = (x+center_x)**2 + (y+center_y)**2 <= 10
-        elif mask_state == 5:
-            mask = (x+center_x)**2 + (y+center_y)**2 >= 2
-        elif mask_state == 6:
-            mask = np.abs(z-1)<=1
-        else:
-            mask = np.ones(len(points), dtype=bool)
-
-        filtered_points = points[mask]
-        
-        for point in filtered_points:
-            distance = np.linalg.norm(point - np.array([center_x, center_y, center_z]))
-            if distance <= min_distance:
-                min_distance = distance
-        
-        print(f"min distance: {min_distance}")
-        with points_lock:
-            latest_points = filtered_points
-            latest_center = np.array([center_x, center_y, center_z])
-
-    except Exception as e:
-        logging.error(f"LiDAR callback error: {e}")
+        except Exception as e:
+            logging.error(f"LiDAR callback error {e}")
+        time.sleep(0.1)
 
 def app_callback(element, buffer, state):
     global center_x, center_y, detected
@@ -169,7 +143,6 @@ def app_callback(element, buffer, state):
 
     detected = True
 
-    #print(f"X0={round(x0, 3)} X1={round(x1, 3)} Y0={round(y0, 3)} Y1={round(y1, 3)}")
 
 def compute_z():
     global center_x
@@ -192,21 +165,22 @@ def compute_y():
         temp_y = 0.05
     elif temp_y < -0.05:
         temp_y = -0.05
-
-    y += temp_y
+    
+    if y <= 0.4 and y >= -0.4:
+        y += temp_y
     
     return y
 
 def compute_x():
     global min_distance
-    deadzone = 0.5
-    target_distance = 4
+    deadzone = 0.1
+    target_distance = 0.3
     x = 0
-
-    if min_distance < target_distance - deadzone:
-        x = (min_distance - target_distance) / 100
-    elif min_distance > target_distance + deadzone:
-        x = (min_distance - target_distance) / 100
+    if min_distance < 10:
+        if min_distance < target_distance - deadzone:
+            x = (min_distance - target_distance)
+        elif min_distance > target_distance + deadzone:
+            x = (min_distance - target_distance)
     else:
         x = 0.0
     
@@ -244,34 +218,34 @@ async def go2_connect():
     await asyncio.sleep(1)
     print("Go2 control loop started")
     while True:
-        print(detected)
-        if True:
+        #print(detected)
+        if detected:
             z = compute_z()
             y = compute_y()
             x = compute_x()
             print(f"min dist: {min_distance}")
             print(f"Z is: {z}")
             print(f"Y is: {y}")
-            print(f"Z is: {z}")
+            print(f"X is: {x}")
 
-            # await conn.datachannel.pub_sub.publish_request_new(
-            #     RTC_TOPIC["SPORT_MOD"],
-            #     {
-            #         "api_id": SPORT_CMD["Move"],
-            #         "parameter": {
-            #             "x": 0.0,
-            #             "y": 0,
-            #             "z": z,
-            #         },
-            #     },
-            # )
-            # await conn.datachannel.pub_sub.publish_request_new(
-            #     RTC_TOPIC["SPORT_MOD"],
-            #     {
-            #         "api_id": SPORT_CMD["Euler"],
-            #         "parameter": {"x": 0, "y": y, "z": 0},
-            #     },
-            # )
+            await conn.datachannel.pub_sub.publish_request_new(
+                RTC_TOPIC["SPORT_MOD"],
+                {
+                    "api_id": SPORT_CMD["Move"],
+                    "parameter": {
+                        "x": x,
+                        "y": 0,
+                        "z": z,
+                    },
+                },
+            )
+            await conn.datachannel.pub_sub.publish_request_new(
+                RTC_TOPIC["SPORT_MOD"],
+                {
+                    "api_id": SPORT_CMD["Euler"],
+                    "parameter": {"x": 0, "y": y, "z": 0},
+                },
+            )
 
         await asyncio.sleep(0.1)
 
@@ -283,6 +257,8 @@ def go2_thread_fn():
 def main():
     t = threading.Thread(target=go2_thread_fn, daemon=True)
     t.start()
+    lidar_thread = threading.Thread(target=lidar_distance, daemon=True)
+    lidar_thread.start()
 
     state = AppState()
     app = GStreamerDetectionApp(app_callback, state)
